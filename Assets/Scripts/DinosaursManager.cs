8f091d3b077478f8a4668eccb1e4c28c1cae7228
Sheikh Mohsin Kader <smk.mohsin@gmail.com>
2022-09-12 12:54:26 +0530

    Final Commit

---
+++ Assets/Scripts/DinosaursManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DinosaursManager : MonoBehaviour
+{
+    int _index = 0;
+
+    private void Awake()
+    {
+        ChangeDinosaur(0);
+    }
+
+    public void ChangeDinosaur(int _change)
+    {
+        if (_index == transform.childCount-1 && _change == 1)
+            _index = 0;
+        else if (_index == 0 && _change == -1)
+            _index = transform.childCount-1;
+        else
+            _index += _change;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(i == _index);
+        }
+    }
+}
